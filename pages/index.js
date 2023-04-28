45e2e0cabcd39e6dcb4d6f537fa17e629afaffbd
dinafoley <dinam48@gmail.com>
2023-04-28 12:55:44 +0600

    menu+shipping+loginn+db

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+
+import Layout from '../components/Layout';
+import ItemPage from '../components/ItemPage';
+import Product from '../models/Product';
+import db from '../utils/db';
+import { Store } from '../utils/Store';
+import axios from 'axios';
+import { useContext } from 'react';
+import { toast } from 'react-toastify';
+
+
+
+export default function Home({ products }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock < quantity) {
+      return toast.error('Sorry. Product is out of stock');
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+
+    toast.success('Product added to the cart');
+  };
+
+    return (
+        <Layout title="Home Page">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {products.map((product) => (
+          <ItemPage
+            product={product}
+            key={product.slug}
+            addToCartHandler={addToCartHandler}
+          ></ItemPage>
+            ))}
+          </div>
+        </Layout>
+      );
+ 
+}
+export async function getServerSideProps() {
+    await db.connect();
+    const products = await Product.find().lean();
+    return {
+      props: {
+        products: products.map(db.convertDocToObj),
+      },
+    };
+  }
